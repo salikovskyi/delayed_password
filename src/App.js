79801ebd757912b9d6f9e01894568d5e301c1611
salikovskyi <osalikovsiy@gmail.com>
2022-08-05 09:36:39 +0300

    o

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import "./App.css";
 
 const func = (pattern, num) => {
@@ -19,36 +19,39 @@ function App() {
     return () => window.clearTimeout(timer);
   }, [password]);
 
-  const handlePasswordChange = (e) => {
-    const inputText = e.target.value;
-    const index = e.target.selectionStart;
-    const addedTextLength = inputText.length - unmaskedPassword.length;
-    const input = document.querySelector(".input");
+  const handlePasswordChange = useCallback(
+    (e) => {
+      const inputText = e.target.value
+      const index = e.target.selectionStart;
+      const addedTextLength = inputText.length - unmaskedPassword.length;
 
-    if (index > 12) {
-      setError(true);
-      input.setAttribute("maxlength", "12");
-    } else {
-      setError(false);
-    }
+      if (index > 11) {
+        setError(true);
+      } else {
+        setError(false);
+      }
 
-    if (inputText.length === 0) {
-      setPassword("");
-    } else {
-      setPassword(
-        func("●", inputText.length - 1) + inputText.charAt(inputText.length - 1)
-      );
-    }
+      if (inputText.length === 0) {
+        setPassword("");
+      } else {
+        setPassword(
+          func("●", inputText.length - 1) +
+            inputText.charAt(inputText.length - 1)
+        );
+        console.log(inputText);
+      }
 
-    if (addedTextLength > 0) {
-      const newStr = inputText.slice(index - addedTextLength, index);
-      setUnmaskedPassword(
-        unmaskedPassword.slice(0, index - addedTextLength) +
-          newStr +
-          unmaskedPassword.slice(index - addedTextLength)
-      );
-    }
-  };
+      if (addedTextLength > 0) {
+        const newStr = inputText.slice(index - addedTextLength, index);
+        setUnmaskedPassword(
+          unmaskedPassword.slice(0, index - addedTextLength) +
+            newStr +
+            unmaskedPassword.slice(index - addedTextLength)
+        );
+      }
+    },
+    [password]
+  );
 
   return (
     <div>
@@ -57,8 +60,11 @@ function App() {
         value={password}
         onChange={handlePasswordChange}
         className="input"
+        maxLength="12"
       />
-      {error ? <p className="error"> Пароль дуже довгий</p> : null}
+      {error ? (
+        <p className="error"> Пароль може бути не більше 12 символів</p>
+      ) : null}
       {password ? <p>Ваш Пароль - {unmaskedPassword}</p> : null}
     </div>
   );
